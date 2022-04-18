f6a82400fc93d963da9725187b189bf9768b548f
rookie-academy <newsubmission.ra@gmail.com>
2022-04-18 23:29:52 +0800

    config

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,15 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addWatchTarget("./src/sass/");
 
     eleventyConfig.addPassthroughCopy("./src/css");
+
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATETIME_MED);
+    });
 
     return {
         dir: "src",
---
+++ src/blog/2022-04-17-yet-another-post.md
@@ -0,0 +1,12 @@
+---
+title: Yet Another post
+description: It is  an awesome post!
+author: Peter Jones
+date: 2022-04-10T14:32:00.000Z
+tags:
+  - post
+  - featured
+image: //unsplash.it/354
+imageAlt: Awesome photo
+---
+Try a new **[post](https://www.facebook.com/rookieacademyaaa)**!
