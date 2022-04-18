4978a3fad0875bfaee1eeabf6dcd847aa7ea0f26
rookie-academy <newsubmission.ra@gmail.com>
2022-04-18 22:04:05 +0800

    sass config

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function(eleventyConfig) {
-    
+    eleventyConfig.addWatchTarget("./src/sass/");
+
+    eleventyConfig.addPassthroughCopy("./src/css");
+
     return {
         dir: "src",
         output: "public",
